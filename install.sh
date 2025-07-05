8508d6b30427bd223221dd1418257436052a56cc
Rainbownx <nohramkissoon1@gmail.com>
2025-07-05 16:26:48 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,5 +8,5 @@ sudo git clone https://github.com/rainbownx/PackMana.git "$TARGET_DIR"
 
 # Link the main executable to /usr/bin
 sudo ln -sf "$TARGET_DIR/packmana" /usr/bin/packmana
-
+sudo chmod +x /usr/bin/packmana
 echo "Packmana installed. You can run it by typing 'packmana'."
